27e1af885347dfe90abf11bae97cfa2e5867acec
44mira <legolaslada@gmail.com>
2024-12-25 20:17:33 +0800

    feat: folds

--- init.lua
+++ init.lua
@@ -30,6 +30,25 @@ vim.opt.scrolloff = 2
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- Use global statusline
+vim.o.laststatus = 3
+
+-- Use marker folds
+vim.o.foldmethod = 'marker'
+
+FoldText = function()
+  local foldStart = vim.fn.getline(vim.v.foldstart)
+  -- formatting block {{{1
+  local title = foldStart:gmatch('%s*[^%s]* (.+) {{{.*')
+  local icon = "🟆 "
+  -- }}}1
+  return icon .. ('[[ %s ]]'):format(title())
+end
+
+vim.o.foldlevelstart = 0
+vim.o.foldtext = 'v:lua.FoldText()'
+
+-- autocommands {{{
 -- highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = "Highlight text when yanking",
@@ -38,6 +57,26 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end
 })
+
+-- create augroup for fold states
+vim.api.nvim_create_augroup('vivi-fold-state', { clear = true })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  desc = "Save fold state when saving",
+  group = 'vivi-fold-state',
+  callback = function()
+    vim.cmd "mkview!"
+  end
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  desc = "Load fold state when reading",
+  group = 'vivi-fold-state',
+  callback = function()
+    vim.cmd "loadview"
+  end
+})
+-- }}}
 
 require('vivi.keymaps')
 require('vivi.lazy')
