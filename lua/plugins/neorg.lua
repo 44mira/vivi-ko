2ca0b848a956ef4ff1cd1942bd82c0a5e07f5dd0
44mira <legolaslada@gmail.com>
2025-03-11 02:28:52 +0800

    feat: qol and export

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -8,15 +8,22 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-neorg/neorg-telescope",
+		"benlubas/neorg-conceal-wrap",
 		"folke/zen-mode.nvim",
+		"dhruvasagar/vim-table-mode",
 	},
 	opts = {
+		-- Plugins {{{
 		load = {
-			["core.clipboard.code-blocks"] = {},
-			["core.ui"] = {},
-			["core.integrations.treesitter"] = {},
 			["core.defaults"] = {},
+			["core.clipboard.code-blocks"] = {},
+			["core.integrations.treesitter"] = {},
 			["core.concealer"] = {},
+			["core.qol.toc"] = {
+				config = {
+					fixed_width = 40,
+				},
+			},
 			["core.completion"] = {
 				config = {
 					engine = "nvim-cmp",
@@ -35,13 +42,10 @@ return {
 					default_workspace = "neorg",
 				},
 			},
-			["core.presenter"] = {
-				config = {
-					zen_mode = "zen-mode",
-				},
-			},
+			["core.export"] = {},
 			["core.integrations.telescope"] = {},
-		},
+			["external.conceal-wrap"] = {},
+		}, -- }}}
 	},
 	init = function()
 		vim.keymap.set(
@@ -56,18 +60,14 @@ return {
 		vim.api.nvim_create_autocmd("Filetype", {
 			pattern = "norg",
 			callback = function()
-				vim.keymap.set(
-					"n",
-					"<leader><right>",
-					"<Plug>(neorg.presenter.next-page)",
-					{ desc = "Next slide", buffer = true }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader><left>",
-					"<Plug>(neorg.presenter.previous-page)",
-					{ desc = "Previous slide", buffer = true }
-				)
+				-- vim.g.table_mode_corner = "|"
+
+				local bind = function(lhs, rhs, desc)
+					vim.keymap.set("n", lhs, rhs, { desc = desc, buffer = true })
+				end
+
+				bind("<leader>to", "<CMD>Neorg toc right<CR>", "Table of contents")
+				bind("<leader>co", "<Plug>(neorg.looking-glass.magnify-code-block)", "Use Looking Glass")
 			end,
 		})
 	end,
