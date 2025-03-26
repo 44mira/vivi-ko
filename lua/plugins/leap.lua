b36fe438c87cb717488bcfaed7908bb25532bcc1
44mira <legolaslada@gmail.com>
2025-03-27 03:27:08 +0800

    fix: remove conflict bind for silicon and leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,7 +2,7 @@ return {
 	"ggandor/leap.nvim",
 	init = function()
 		local bind = function(lhs, rhs, desc)
-			vim.keymap.set({ "n", "x", "o" }, lhs, rhs, { desc = desc })
+			vim.keymap.set({ "n", "o" }, lhs, rhs, { desc = desc })
 		end
 
 		bind("<leader><leader>", "<Plug>(leap)")
